60f863bac8779ac1299f282e485d6463bf69f988
Michael Merickel <michael@merickel.org>
2019-04-06 13:02:12 -0500

    fix tutorial_path

--- tests/tutorial/conftest.py
+++ tests/tutorial/conftest.py
@@ -6,5 +6,8 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def docs_path():
-    tutorial_path = os.path.relpath('../../docs/tutorial', __file__)
-    sys.path.append(tutorial_path)
+    tutorial_path = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        '../../docs/tutorial',
+    ))
+    sys.path.insert(0, tutorial_path)
