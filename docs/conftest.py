cc9bfe074f2f17251881bc7647ba9a5c783ba913
Paul Everitt <pauleveritt@me.com>
2019-04-27 14:02:17 -0400

    For now, let's only run Sybil on code blocks if we are Python 3.6 or higher in tox.

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,3 +1,4 @@
+import sys
 from doctest import ELLIPSIS
 
 from sybil import Sybil
@@ -5,11 +6,13 @@ from sybil.parsers.codeblock import CodeBlockParser
 from sybil.parsers.doctest import DocTestParser, FIX_BYTE_UNICODE_REPR
 from sybil.parsers.skip import skip
 
-pytest_collect_file = Sybil(
-    parsers=[
-        DocTestParser(optionflags=ELLIPSIS | FIX_BYTE_UNICODE_REPR),
-        CodeBlockParser(future_imports=['print_function']),
-        skip
-    ],
-    pattern='index.rst',
-).pytest()
+if sys.version_info >= (3, 6):
+    pytest_collect_file = Sybil(
+        parsers=[
+            DocTestParser(optionflags=ELLIPSIS | FIX_BYTE_UNICODE_REPR),
+            CodeBlockParser(future_imports=['print_function']),
+            skip
+        ],
+        pattern='index.rst',
+        excludes=['docs/tutorials/dcdi/* ']
+    ).pytest()
