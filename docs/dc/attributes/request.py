4a63886d40a366b3aca07f81e33683f61718a403
Paul Everitt <pauleveritt@me.com>
2019-07-15 17:50:20 -0400

    Don't use container.set, instead, use register_singleton

--- docs/dc/attributes/request.py
+++ docs/dc/attributes/request.py
@@ -1,14 +1,15 @@
 # request.py
+from wired import ServiceContainer
 from .models import Greeter, Request
 
 
 def process_request(registry, name):
     # Each request gets its own container
-    container = registry.create_container()
+    container: ServiceContainer = registry.create_container()
 
     # Manually make a Request and put in the container
     request = Request(url=name)
-    container.set(request, Request)
+    container.register_singleton(request, Request)
 
     # Now get the Greeter, which get Context, which uses the factory
     greeter = container.get(Greeter)
