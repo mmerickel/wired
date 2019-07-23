b968fe1939ba6f0a841c05418ecd651d4f88af20
Paul Everitt <pauleveritt@me.com>
2019-07-23 09:27:18 -0400

    Unused container.

--- docs/dc/simple_construction/configure.py
+++ docs/dc/simple_construction/configure.py
@@ -1,9 +1,8 @@
 # configure.py
-from .models import Greeter
 from wired import ServiceRegistry
 from wired.dataclasses import register_dataclass
+from .models import Greeter
 
 
 def register(registry: ServiceRegistry):
-    container = registry.create_container()
     register_dataclass(registry, Greeter)
