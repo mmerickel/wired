fbe45db334cdeca6f6e01287b64696c6a7e21719
Paul Everitt <pauleveritt@me.com>
2020-06-29 10:04:47 -0400

    Stop Sphinx from complaining.

--- src/wired/dataclasses/__init__.py
+++ src/wired/dataclasses/__init__.py
@@ -1,6 +1,6 @@
+__all__ = ['Context', 'factory', 'injected', 'register_dataclass', 'singleton']
+
 from .decorators import factory, singleton
 from .field_types import injected
 from .models import Context
 from .registration import register_dataclass
-
-__all__ = [Context, factory, injected, register_dataclass, singleton]
